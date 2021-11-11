d89b1f6f3544f7cf7ffb41f5837cc493bfff6160
CRISPYTYPER <jack3409@naver.com>
2021-11-11 20:12:07 +0900

    modified comments in MessageRepository

--- src/main/java/app/messages/MessageRepository.java
+++ src/main/java/app/messages/MessageRepository.java
@@ -5,8 +5,8 @@ import org.apache.commons.logging.LogFactory;
 
 public class MessageRepository {
     private final static Log log = LogFactory.getLog(MessageRepository.class);
-    //static 으로 선언된 변수는 메모리 공간에 하나만 존재하며, 어디서나 접근이 가능한 변수이다.
-
+    //static으로 선언된 변수는 메모리 공간에 하나만 존재하며, 어디서나 접근이 가능한 변수이다.
+    //final은 변수를 상수화 시킨다. 즉, 값의 변경이 불가능하다.
 
     public void saveMessage(Message message) {
         // 데이터베이스에 메시지를 저장한다.
