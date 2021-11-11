bced251792ff4aaa93abc08b57e85ad1351af9a7
CRISPYTYPER <jack3409@naver.com>
2021-11-11 20:19:12 +0900

    add MessageService.java

--- src/main/java/app/messages/MessageRepository.java
+++ src/main/java/app/messages/MessageRepository.java
@@ -3,7 +3,7 @@ package app.messages;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-public class MessageRepository {
+public class MessageRepository { //메시지 저장을 담당하는 레포지토리
     private final static Log log = LogFactory.getLog(MessageRepository.class);
     //static으로 선언된 변수는 메모리 공간에 하나만 존재하며, 어디서나 접근이 가능한 변수이다.
     //final은 변수를 상수화 시킨다. 즉, 값의 변경이 불가능하다.
--- src/main/java/app/messages/MessageService.java
+++ src/main/java/app/messages/MessageService.java
@@ -1,5 +1,14 @@
 package app.messages;
 
 public class MessageService {
+    private MessageRepository repository;
+    // MessageRepository를 MessageService의 의존성으로 정의한다.
 
+    public MessageService(MessageRepository repository) { //스프링은 이 생성자를 통해 의존성을 연결한다.
+        this.repository = repository;
+    }
+    public void save(String text) {
+        this.repository.saveMessage(new Message(text));
+        //클라이언트가 String 타입의 text를 전달해 레포지토리에 저장하는 단순한 API인 save(String) 메소드를 정의한다.
+    }
 }
